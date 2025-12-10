c6790f20fd2d6bae86a6f866b3644fec77dc9b9a
van06-0419 <zhiyuan.wang@yandex.ru>
2025-12-10 15:37:06 +0700

    Update EditorView.cpp

--- src/EditorView.cpp
+++ src/EditorView.cpp
@@ -5,26 +5,26 @@ EditorView::EditorView(std::shared_ptr<EditorController> ctrl)
     : controller(ctrl) {}
 
 void EditorView::onNewDocument() {
-    std::cout << "[GUI] User clicked 'New Document'" << std::endl;
+    std::cout << "[GUI] Пользователь нажал 'Создать документ'" << std::endl;
     controller->createNewDocument();
 }
 
 void EditorView::onImport(const std::string& path) {
-    std::cout << "[GUI] User clicked 'Import Document'" << std::endl;
+    std::cout << "[GUI] Пользователь нажал 'Импорт документа'" << std::endl;
     controller->importDocument(path);
 }
 
 void EditorView::onExport(const std::string& path) {
-    std::cout << "[GUI] User clicked 'Export Document'" << std::endl;
+    std::cout << "[GUI] Пользователь нажал 'Экспорт документа'" << std::endl;
     controller->exportDocument(path);
 }
 
 void EditorView::onCreateShape(const std::string& type) {
-    std::cout << "[GUI] User created shape: " << type << std::endl;
+    std::cout << "[GUI] Пользователь создал фигуру: " << type << std::endl;
     controller->createShape(type);
 }
 
 void EditorView::onDeleteShape(int id) {
-    std::cout << "[GUI] User deleted shape #" << id << std::endl;
+    std::cout << "[GUI] Пользователь удалил фигуру #" << id << std::endl;
     controller->deleteShape(id);
 }
